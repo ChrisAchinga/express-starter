babb7f327a0b0de009b47b3fe0fba8ef6521eb92
ChrisAchinga <achinga.chris@gmail.com>
2020-11-08 18:07:12 +0300

    app server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("testing endpont");
+});
+
+app.listen(PORT, () => console.log(`app running on port ${PORT}`));
