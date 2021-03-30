67b68f7facd9e591ee66eafe9514f0e3d02745c3
ChrisAchinga <achinga.chris@gmail.com>
2021-03-30 13:56:00 +0300

    install express

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const PORT = 3000;
+const PORT = 3000
 
-app.get("/", (req, res) => {
-  res.send("testing endpont");
-});
+app.get('/', (req, res) => {
+  res.send('testing endpont')
+})
 
-app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+app.listen(PORT, () => console.log(`app running on port ${PORT}`))
